222238a0648436e5184875532101acb342eb53da
Tom Portegys <portegys@gmail.com>
2020-02-28 16:57:26 -0600

    Test foraging RNN script

--- work/test_hive_size.sh
+++ work/test_hive_size.sh
@@ -3,10 +3,10 @@ trials=10
 echo "Test hive size"
 echo Trials = $trials
 hiveRadius=2
-echo test hiveRadius = #hiveRadius
+echo test hiveRadius = $hiveRadius
 for (( i=0; i< $trials; ++i)); do
     echo trial = $i
-    ./honey_bees.bat -randomSeed $i -saveNN nn.dat -steps 20000 -display false -printCollectedNectar -hiveRadius $hiveRadius
-    ./honey_bees.bat -randomSeed $i -driver metamorphNN -loadNN nn.dat -steps 20000 -display false -printCollectedNectar -hiveRadius $hiveRadius
+    ./honey_bees.sh -randomSeed $i -saveNN nn.dat -steps 20000 -display false -printCollectedNectar -hiveRadius $hiveRadius
+    ./honey_bees.sh -randomSeed $i -driver metamorphNN -loadNN nn.dat -steps 20000 -display false -printCollectedNectar -hiveRadius $hiveRadius
 done
 
