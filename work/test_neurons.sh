7c7b127b44a15cea8e66a8df85bbca6bde9e910a
Tom Portegys <portegys@gmail.com>
2020-02-29 14:48:34 -0600

    Extend neuron test script

--- work/test_neurons.sh
+++ work/test_neurons.sh
@@ -9,4 +9,11 @@ for (( i=0; i< $trials; ++i)); do
     ./honey_bees.sh -randomSeed $i -saveNN nn.dat -steps 20000 -display false -printCollectedNectar -NNhiddenLayers $hiddenNeurons
     ./honey_bees.sh -randomSeed $i -driver metamorphNN -loadNN nn.dat -steps 20000 -display false -printCollectedNectar -NNhiddenLayers $hiddenNeurons
 done
+hiddenNeurons="100"
+echo test hiddenNeurons = $hiddenNeurons
+for (( i=0; i< $trials; ++i)); do
+    echo trial = $i
+    ./honey_bees.sh -randomSeed $i -saveNN nn.dat -steps 20000 -display false -printCollectedNectar -NNhiddenLayers $hiddenNeurons
+    ./honey_bees.sh -randomSeed $i -driver metamorphNN -loadNN nn.dat -steps 20000 -display false -printCollectedNectar -NNhiddenLayers $hiddenNeurons
+done
 
