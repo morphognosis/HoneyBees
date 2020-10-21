24b46ac409639f8a70dcb9a29d92aeb361fb6dac
Tom Portegys <portegys@gmail.com>
2020-10-20 22:53:54 -0500

    Python to src

--- work/build.bat
+++ work/build.bat
@@ -3,4 +3,5 @@ copy ..\res\images\honeybee.png morphognosis\honey_bees
 copy ..\res\images\flower.png morphognosis\honey_bees
 copy ..\res\images\nectar.png morphognosis\honey_bees
 copy ..\res\sounds\bees.wav morphognosis\honey_bees
+copy ..\src\morphognosis\honey_bees\foraging_rnn.py .
 jar cvfm ../bin/honey_bees.jar honey_bees.mf morphognosis de foraging_rnn.py
--- work/build.sh
+++ work/build.sh
@@ -4,4 +4,5 @@ cp ../res/images/honeybee.png morphognosis/honey_bees
 cp ../res/images/flower.png morphognosis/honey_bees
 cp ../res/images/nectar.png morphognosis/honey_bees
 cp ../res/sounds/bees.wav morphognosis/honey_bees
+cp ../src/morphognosis/honey_bees/foraging_rnn.py .
 jar cvfm ../bin/honey_bees.jar honey_bees.mf morphognosis de foraging_rnn.py
